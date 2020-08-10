b7b57df9ff921e42eaf5c0797d875a56e74a320c
Danniell Hu <dannihu01@gmail.com>
2020-08-10 18:53:43 -0400

    Deleted credentials and unnecessary ID

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -6,33 +6,39 @@ import datetime as dt
 
 reddit_wrap = praw.Reddit(client_id='fqqAzLDxlZ86-A', \
                             client_secret='GCtXSXE0V4eMZvY9GMHipmvwgNg',\
-                                user_agent = 'Reddit_Scraper',\
-                                    username='NonlinearApplet',\
-                                        password = 'tepidbunny2001@')
+                                user_agent = 'Reddit_Scraper')
 
 
-subreddit = reddit_wrap.subreddit('indieheads')
+
+subreddit = reddit_wrap.subreddit('EDM')
 
 top_subreddit = subreddit.hot(limit = 100)
 
-for submission in subreddit.hot(limit = 10):
-    print(submission.title, submission.id)
-    print (' ')
+
 
 topics_dict = {
     "title":[],\
-        "id":[],\
             "url":[],\
-                "created":[], \
-                    "body":[]
+                "body":[], 
+                    "created":[]
 }
 
 for submission in top_subreddit:
     topics_dict["title"].append(submission.title)
-    topics_dict["id"].append(submission.id)
     topics_dict["url"].append(submission.url)
-    topics_dict["created"].append(submission.created)
     topics_dict["body"].append(submission.selftext)
+    topics_dict["created"].append(submission.created)
 
 
 topics_data = pd.DataFrame(topics_dict)
+
+def get_date(created):
+    return dt.datetime.fromtimestamp(created)
+
+
+_timestamp = topics_data["created"].apply(get_date)
+
+topics_data = topics_data.assign(timestamp = _timestamp)
+
+
+topics_data.to_csv('Reddit_Scraper.csv', index=False)
